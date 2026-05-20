fa25d03d6a68a6d64ae0cb756fccc3b707dabe9f
Aryan <aryanlytics@gmail.com>
2026-05-20 19:35:36 +0500

    .

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -3,6 +3,18 @@ require("dotenv").config();
 if (!process.env.MONGODB_URI) {
   throw new Error("MONGODB_URI is not defined in the .env file");
 }
+if (!process.env.REDIS_HOST) {
+  throw new Error("REDIS_HOST is not defined in the .env file");
+}
+if (!process.env.REDIS_PORT) {
+  throw new Error("REDIS_PORT is not defined in the .env file");
+}
+if (!process.env.REDIS_USERNAME) {
+  throw new Error("REDIS_USERNAME is not defined in the .env file");
+}
+if (!process.env.REDIS_PASSWORD) {
+  throw new Error("REDIS_PASSWORD is not defined in the .env file");
+}
 
 if (!process.env.JWT_SECRET) {
   throw new Error("JWT_SECRET is not defined in the .env file");
@@ -11,6 +23,10 @@ if (!process.env.JWT_SECRET) {
 const config = {
   MONGODB_URI: process.env.MONGODB_URI,
   JWT_SECRET: process.env.JWT_SECRET,
+  REDIS_USERNAME: process.env.REDIS_USERNAME,
+  REDIS_HOST: process.env.REDIS_HOST,
+  REDIS_PASSWORD: process.env.REDIS_PASSWORD,
+  REDIS_PORT: process.env.REDIS_PORT,
 };
 
 module.exports = config;
--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -2,10 +2,13 @@ const express = require("express");
 const { register, login } = require("../controllers/authController");
 const { loginLimiter } = require("../middleware/rateLimiter");
 const { protect } = require("../middleware/auth");
+const validate = require("../middleware/validate");
+const { registerSchema } = require("../validators/authValidation");
 
 const router = express.Router();
 
-router.post("/register", register);
+router.post("/register", validate(registerSchema), register);
+
 router.post("/login", loginLimiter, login); // ← Rate limiter applied here
 
 module.exports = router;
